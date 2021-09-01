64ed5e6e9ceb2827af296340371cde333a08c564
Denis Mythnik <themrcrovvley@gmail.com>
2021-09-01 11:58:34 +0300

    feat: task solved using RPN

--- src/index.js
+++ src/index.js
@@ -1,212 +1,141 @@
-// function eval() {
-//     // Do not use eval!!!
-//     return;
-// }
+function eval() {
+    // Do not use eval!!!
+    return;
+}
 
-// const { sort } = require("semver");
+const { sort } = require("semver");
 
-// function expressionCalculator(expr) {
-//     (function() {
-//         let openBracketCounter = 0;
-//         let closeBracketCounter = 0;
-//         expr.split('').forEach(token => {
-//             if(token === '(') {
-//                 openBracketCounter++;
-//             }
-//             if(token === ')') {
-//                 closeBracketCounter++;
-//             }
-//             if(openBracketCounter !== closeBracketCounter) {
-//                 throw new Error("ExpressionError: Brackets must be paired");
-//             }
-//         });
-//     })();
-
-//     function correctArray() {
-//         const exprArr = expr.split('');
-//         let num = '';
-//         let resultingArray = [];
-    
-//         exprArr.forEach(item => {
-//             if(item && item !== ' ') {
-//                 if (!isNaN(item)) {
-//                     num += item;
-//                 }
-//                 if (isNaN(item)) {
-//                     if (num) {
-//                         resultingArray.push(num);
-//                     }
-//                     resultingArray.push(item);
-//                     num = '';
-//                 }
-//             }
-//         });
-//         if (num) {
-//             resultingArray.push(num);
-//         }
-//         return resultingArray;
-//     }
-
-//     function sortingStation() {
-//         const arr = correctArray();
-//         const operationsPriority = {
-//                     '-': 1,
-//                     '+': 1,
-//                     '*': 2,
-//                     '/': 2,
-//                 };
-    
-//         let res = '';
-//         let stack = [];
-//         arr.forEach(item => {
-//             if(item !== ' ') {
-//                 const stackPeak = stack[stack.length - 1];
-//                 if(!isNaN(item)) {
-//                     res += `${item} `;
-//                 } else if(!stack.length) {
-//                     stack.push(item);
-//                 } else if(operationsPriority[stackPeak] === operationsPriority[item]) {
-//                     res += `${stack.pop()} `;
-//                     stack.push(item);
-//                 } else if(operationsPriority[stackPeak] > operationsPriority[item]) {
-//                     res += `${stack.pop()} `;
-//                     res += `${item} `;
-//                 }
-//                 else {
-//                     stack.push(item);
-//                 }
-//             }
-//         });
-//         stack = stack.filter(token => token !== '(' && token !== ')');
-//         while(stack.length) {
-//             res += `${stack.pop()} `;
-//         }
-//         return res;
-//     }
-
-//     const operators = {
-//         '+': (x, y) => x + y,
-//         '-': (x, y) => x - y,
-//         '*': (x, y) => x * y,
-//         '/': (x, y) => x / y
-//     };
-    
-//     const reversePolishNotationCalc = () => {
-//         const str = sortingStation();
-//         if(str.includes(' 0 / ')) {
-//             throw new Error("TypeError: Division by zero.");
-//         }
-//         let stack = [];
-//         str.split(' ').forEach((token) => {
-//             if (token in operators) {
-//                 let [y, x] = [stack.pop(), stack.pop()];
-//                 stack.push(operators[token](x, y));
-//             } else {
-//                 stack.push(parseFloat(token));
-//             }
-//         });
-//         console.log(stack);
-
-//         return stack[0];
-//     };
-//     return reversePolishNotationCalc();
-// }
-
-// module.exports = {
-//     expressionCalculator
-// }
-
-function correctArray(expr) {
-    const exprArr = expr.split('');
-    let num = '';
-    let resultingArray = [];
-
-    exprArr.forEach(item => {
-        if(item && item !== ' ') {
-            if (!isNaN(item)) {
-                num += item;
+function expressionCalculator(expr) {
+    // Check paired brackets
+    (function() {
+        let openBracketCounter = 0;
+        let closeBracketCounter = 0;
+        expr.split('').forEach(token => {
+            if(token === '(') {
+                openBracketCounter++;
             }
-            if (isNaN(item)) {
-                if (num) {
-                    resultingArray.push(num);
+            if(token === ')') {
+                closeBracketCounter++;
+            }            
+        });
+        if(openBracketCounter !== closeBracketCounter) {
+            throw new Error("ExpressionError: Brackets must be paired");
+        }
+    })();
+
+    // Transform input string into array without spaces and concat non single numbers
+    function correctArray() {
+        const exprArr = expr.split('');
+        let numberValue = '';
+        let resultingArray = [];
+    
+        exprArr.forEach(item => {
+            if(item && item !== ' ') {
+                if (!isNaN(item)) {
+                    numberValue += item;
                 }
-                resultingArray.push(item);
-                num = '';
+                if (isNaN(item)) {
+                    if (numberValue) {
+                        resultingArray.push(numberValue);
+                    }
+                    resultingArray.push(item);
+                    numberValue = '';
+                }
             }
+        });
+        if (numberValue) {
+            resultingArray.push(numberValue);
         }
-    });
-    if (num) {
-        resultingArray.push(num);
+        return resultingArray;
     }
-    return resultingArray;
-}
-// 20, 57, 12, *, -, 58, 84, 32, *, 27, /, + -
 
-function sortingStation() {
-    const arr = correctArray("20 - 57 * 12 - (  58 + 84 * 32 / 27  )");
-    // console.log(arr);
-    const operationsPriority = {
-                '-': 1,
-                '+': 1,
-                '*': 2,
-                '/': 2,
-                '(': 3,
-                ')': 3
-            };
+    // Get correctArray and transform into Reverse Polish Notation string
+    function sortingStation() {
+        const arr = correctArray();
 
-    let res = '';
-    let stack = [];
-    arr.forEach(item => {
-        if(item !== ' ') {
-            const stackPeak = stack[stack.length - 1];
-            if(!isNaN(item)) {
-                res += `${item} `;
-            } else if(!stack.length) {
-                stack.push(item);
-            } else if(operationsPriority[item] > operationsPriority[stackPeak]) {                
-                stack.push(item);
-            } else if(operationsPriority[item] <= operationsPriority[stackPeak]) {
-                stack.push(item);
+        const operationsPriority = {
+                    '-': 1,
+                    '+': 1,
+                    '*': 2,
+                    '/': 2,
+                };
+
+        let resultString = '';    
+        let stack = [];    
+        arr.forEach(token => {
+            let stackPeak = stack[stack.length - 1];
+            if(!isNaN(token)) {
+                resultString += `${token} `;
+                return;
             }
+            if(token === '(') {
+                stack.push(token);
+                return;
+            }
+            if(token === ')') {
+                while(stackPeak !== '(') {
+                    resultString += `${stack.pop()} `;
+                    stackPeak = stack[stack.length - 1];
+                }
+                stack.pop();
+                return;
+            }
+            if(operationsPriority.hasOwnProperty(token)) {
+                if(!stack.length) {
+                    stack.push(token);
+                    return;
+                }
+                while(operationsPriority[stackPeak] >= operationsPriority[token]) {
+                    if(stackPeak === '(') {
+                        break;
+                    }
+                    resultString += `${stack.pop()} `;
+                    stackPeak = stack[stack.length - 1];
+                }
+                stack.push(token);
+                return;
+            }
+        });
+
+        while(stack.length) {
+            resultString += `${stack.pop()} `;
         }
-        // console.log(stack);
-    });
-    stack = stack.filter(token => token !== '(' && token !== ')');
-    while(stack.length) {
-        res += `${stack.pop()} `;
-    }
-    return res;
+
+        return resultString.substring(0, resultString.length - 1);
+    }    
+    
+    //Get RPN string and calculate result
+    const calculateRPN = () => {
+        const str = sortingStation();
+    
+        const operations = {
+            '+': (x, y) => x + y,
+            '-': (x, y) => x - y,
+            '*': (x, y) => x * y,
+            '/': (x, y) => x / y
+        };
+
+        // Check for division by zero 
+        if(str.includes(' 0 /')) {
+            throw new Error("TypeError: Division by zero.");
+        }
+    
+        let stack = [];
+        str.split(' ').forEach((token) => {
+            if (token in operations) {
+                let [y, x] = [stack.pop(), stack.pop()];
+                stack.push(operations[token](x, y));
+            } else {
+                stack.push(parseFloat(token));
+            }
+        });
+    
+        return stack.pop();
+    };
+
+    return calculateRPN();
 }
 
-console.log(sortingStation());
-
-// const operators = {
-//     '+': (x, y) => x + y,
-//     '-': (x, y) => x - y,
-//     '*': (x, y) => x * y,
-//     '/': (x, y) => x / y
-// };
-
-// const reversePolishNotationCalc = () => {
-//     const str = sortingStation();
-//     if(str.includes(' 0 / ')) {
-//         throw new Error("TypeError: Division by zero.");
-//     }
-//     let stack = [];
-//     str.split(' ').forEach((token) => {
-//         if (token in operators) {
-//             let [y, x] = [stack.pop(), stack.pop()];
-//             stack.push(operators[token](x, y));
-//         } else {
-//             stack.push(parseFloat(token));
-//         }
-//     });
-//     console.log(stack);
-
-//     return stack[0];
-// };
-// // return reversePolishNotationCalc();
-
-// console.log(reversePolishNotationCalc());
-
-// 20, 57, 12, *, -, 58, 84, 32, *, 27, /, +
+module.exports = {
+    expressionCalculator
+};
